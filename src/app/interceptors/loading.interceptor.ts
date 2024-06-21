ca66717e086bfd0789b1e0060ad7934f0197b4ae
Rasula Rathnayake <92834629+RASULAdalf@users.noreply.github.com>
2024-06-20 23:04:33 -0700

    Update loading.interceptor.ts

--- src/app/interceptors/loading.interceptor.ts
+++ src/app/interceptors/loading.interceptor.ts
@@ -1,11 +1,20 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { finalize } from 'rxjs';
+import { Observable, throwError,finalize } from 'rxjs';
+import { catchError, timeout } from 'rxjs/operators';
 import { LoadingService } from '../modules/lessons/services/loading.service';
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const loadingService = inject(LoadingService);
   loadingService.progrssBarLoadingState.next(true);
     
-  return next(req).pipe(finalize(()=> {loadingService.progrssBarLoadingState.next(false);}));
+  return next(req).pipe(
+    timeout(999999),
+      catchError(error => {
+        if (error.name === 'TimeoutError') {
+          return throwError(new Error('Request timed out'));
+        }
+        return throwError(error);
+      }),
+    finalize(()=> {loadingService.progrssBarLoadingState.next(false);}));
 };
 
